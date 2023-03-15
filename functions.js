bb278b510df5689ae1e403af51a17c8cae7f8f75
Giovannyf99 <giovannyf1999@gmail.com>
2023-03-15 09:20:05 -0400

    inital commit

---
+++ functions.js
@@ -0,0 +1,88 @@
+//CELSIUS TO FAHRENHEIT CONVERSATION
+// function convertC(celsius) {
+//     let temp = 20
+//     let fahrenheit = temp * 9/5 + 32;
+//     console.log(fahrenheit)
+//     return fahrenheit;
+
+// }
+// convertC();
+
+//FAHRENHEIT TO CELSIUS CONVERSION
+// function convertF(temp){
+//     let fahrenheit = 50
+//     let celsius = (fahrenheit - 32) * 5/9
+//     console.log(celsius)
+//     return fahrenheit;
+// }
+// convertF();
+
+// IS_EVEN FUNCTION
+// function num(){
+//     let number = 14
+//     if (number % 2 ===0 ){
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+// console.log(num())
+
+//IS_ODD FUNCTION
+//NOT SURE WHY NOT WORKING, ASK FOR HELP
+// function num() {
+//     let number = 13
+//     if (number % 2 === 1) {
+//         return odd;
+//     } else {
+//         return even;
+//     }
+// }
+// console.log(num())
+
+//ONLY_EVENS FUNCTION
+// let onlyEvens = [11, 20, 42, 97, 23, 10]
+// let numbers = onlyEvens.forEach(number => {
+//     if(number % 2 === 0){
+//         console.log(number)
+//     }
+// })
+
+// PRINT NUMBERS
+// let printNumbers = [1,2,3,4,5,6,7,8,9,10]
+// let numbers = num => {
+//     for (let i =1; i < 10; i++){
+
+//     }
+// }
+// console.log(printNumbers)
+
+// JUST THE POSITIVES FOR NEXT THREE
+// let positiveNumbers = [1, -3, 5, -3, 0]
+// let numbers = positiveNumbers.forEach(number => {
+//     if(number >= 0) {
+//         console.log(number)
+//     }
+// })
+
+// let positiveNumbers = [1,2,3]
+// let numbers = positiveNumbers.forEach(number => {
+//     if(number >= 0) {
+//         console.log(number)
+//     }
+// })
+
+// let positiveNumbers = [-1,-2,-3]
+// let numbers = positiveNumbers.forEach(number => {
+//     if(number >= 0) {
+//         console.log(number)
+//     }else{
+//         console.log([])
+//     }
+// })
+
+// SUM THE NUMBERS
+//  let goodNumber = [1,4,8]
+//  let numbers = goodNumber.sum(number => {
+//     console.log(number)
+//  })
